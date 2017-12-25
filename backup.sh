a7510cc6733fa1fdc699f12be1b48e46237428f2
wanghongxu <wang19930902@gmail.com>
2017-12-25 10:59:18 +0800

    backup git

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ cp /var/lib/ghost/data/ghost.db /var/lib/backup-site/ghost-$(date +$FMT).db
 bypy upload /var/lib/backup-site/ghost-$(date +$FMT).db blog/ghost-$(date +$FMT).db
 
 
-tar -C /var/gerrit/review_site -czf /var/lib/backup-site/git-$(date +$FMT).tar.gz git
+tar -C /var/lib/gerrit/data -czf /var/lib/backup-site/git-$(date +$FMT).tar.gz git
 bypy upload /var/lib/backup-site/git-$(date +$FMT).tar.gz git/git-$(date +$FMT).tar.gz
 
 rm -f *.tar.gz
